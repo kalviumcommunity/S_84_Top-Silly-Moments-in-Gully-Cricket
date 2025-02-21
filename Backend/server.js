1a91e3b8f357886889a526377c4f436381144591
name <karankarandevgan@gmail.com>
2025-02-21 09:52:54 +0530

    MongoDB Connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,29 @@
-import express from 'express';
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
 const PORT = 7856;
 
 app.use(express.json());
 
-app.get('/ping' , (req , res) => {
-    res.send('This is Home Route')
-})
+//  Connection with MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
 
-app.listen(PORT , () => {
-    console.log(`Server is running at : http://localhost:${PORT}`)
-})
+// Home Router with database connection status
+app.get("/", (req, res) => {
+  const connectionStatus =
+    mongoose.connection.readyState === 1 ? "Connected" : "Not Connected";
+  res.json({ database_status: connectionStatus });
+});
+app.get("/ping", (req, res) => {
+  res.send("This is Home Route");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running at : http://localhost:${PORT}`);
+});
