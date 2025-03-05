d8c8ed06ff6c5b25f0d3b355de00ea87c91317bf
name <karankarandevgan@gmail.com>
2025-03-05 10:38:29 +0530

    react-app-landing-page

---
+++ Frontend/client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="landingpage">
+      <h1> Top Silly Moments in Gully Cricket</h1>
+      <h2>
+        {" "}
+        Welcome to the most hilarious and entertaining moments from the streets
+        of Gully cricket!{" "}
+      </h2>
+      <h2>
+        Watch , laugh and relive some of the funniest cricketing moments with
+        your friends and family.
+      </h2>
+      <h2>Contact: karan.devgan@example.com</h2>
+    </div>
+  );
+};
+
+export default App;
