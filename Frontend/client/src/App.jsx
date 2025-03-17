0c797e8f86c0cffca6793309fe9cd2c3d40a545f
name <karankarandevgan@gmail.com>
2025-03-17 10:48:04 +0530

    Rendering done for dummy component

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import "./App.css";
-
+import EntityComponent from "./components/EntityCard";
 const App = () => {
   return (
-    <div className="landingpage">
-      <h1> Top Silly Moments in Gully Cricket</h1>
-      <h2>
-        {" "}
-        Welcome to the most hilarious and entertaining moments from the streets
-        of Gully cricket!{" "}
-      </h2>
-      <h2>
-        Watch , laugh and relive some of the funniest cricketing moments with
-        your friends and family.
-      </h2>
+    <div className="">
+      <section id="home-section">
+        {/* title */}
+        {/* subtitle */}
+        <h1> Top Silly Moments in Gully Cricket</h1>
+        <h2 className="sub-title">
+          Welcome to the most hilarious and entertaining moments from the
+          streets of Gully cricket!
+        </h2>
+      </section>
+
+      <section id="moments-section">
+        {/* heading (top silly moments) */}
+        <h2>Watch, laugh and relive!</h2>
+        <div id="cards">
+          <EntityComponent />
+        </div>
+        {/* container of cards [card] */}
+      </section>
+
       <h2>Contact: karan.devgan@example.com</h2>
     </div>
   );
